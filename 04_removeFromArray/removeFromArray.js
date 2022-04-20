9f220e79843e0d0d340926d7ab27640f0860bb5d
Tobi Oyero <tobioyero@gmail.com>
2022-04-20 19:27:27 -0400

    Initial attempt at removeFromArray.js and removed second test constraint

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...moreArgs) {
 
+    // take array
+    let original = arr;
+
+    // take values
+    let args = moreArgs;
+
+    // get index of values
+    for (let i = 0; i < args.length; i++){
+        let index = original.indexOf(args[i]);
+        if (index > -1){
+        original.splice(index, 1);
+        }
+    } 
+    
+    // return new array
+    return original;
 };
 
 // Do not edit below this line
