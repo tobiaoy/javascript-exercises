80af1ed419bab7f5bdc53e9a9a9b3305be5a067e
Tobi Oyero <tobioyero@gmail.com>
2022-04-20 13:25:20 -0400

    added repeatString.js and updated repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,24 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, num) {
+    let str = '';
+    const number = Math.floor(Math.random() * 1000);
+
+    if (num < 0) {
+        return "ERROR"
+   } else if (num === 0 || string === ''){
+       return "";
+   } else if (string != '' && num > 0){
+       for (let i = 1; i <= num; i++){
+           str += string;
+           }
+           return str;
+   } else if (typeof(num) === undefined){
+       for (let i = 1; i<= number; i++){
+           str += string;
+       }
+       return str;
+   }
 };
 
 // Do not edit below this line
