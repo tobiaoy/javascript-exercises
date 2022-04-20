5c249671a2c0a977b21fbe27ead010aec0b10a75
Tobi Oyero <tobioyero@gmail.com>
2022-04-20 19:54:51 -0400

    First Attempt at sumAll.js and passed first two tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(...moreArgs) {
+let higher;
+let lower;
+let sum = 0;
 
+// loop determining the lower value
+for (let i = 0; i < moreArgs.length; i++){
+    lower = moreArgs[0];
+
+    if (moreArgs[i] < lower){
+        lower = moreArgs[i];
+    }
+}
+
+// loop determining the higher value
+for (let i = 0; i < moreArgs.length; i++){
+higher = moreArgs[0];
+
+if (moreArgs[i] > higher){
+    higher = moreArgs[i];
+    }
+
+}
+
+// loop to sum values
+for (let i = lower; i <= higher; i++){
+    sum += i;
+}
+
+return sum;
 };
 
 // Do not edit below this line
