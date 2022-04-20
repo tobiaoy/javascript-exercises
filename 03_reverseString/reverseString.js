1353cf87b3dd0d986358472fcc54296ad6d1409a
Tobi Oyero <tobioyero@gmail.com>
2022-04-20 13:28:13 -0400

    first attempt at string reversal on reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,22 @@
-const reverseString = function() {
+// A function to reverse any string
 
+const reverseString = function(string) {
+// take the string
+const str = string
+
+// split the string 
+split_str = str.split("");
+let reverse_str = [];
+
+// reverse the splits 
+for (let i = split_str.length; i >= 0; i--){
+    reverse_str.push(split_str[i]);
+}
+
+// put the string back together
+let rev_str = reverse_str.join();
+
+return rev_str;
 };
 
 // Do not edit below this line
