cd2a6449f27ef9092d25118b9614dd83d7631dde
Tobi Oyero <tobioyero@gmail.com>
2022-04-21 19:02:59 -0400

    first attempt at fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const loopCount = num;
+    let fib = [];
 
+    if (num === 1 || num === 2){
+        return 1
+    } else {
+        fib.push(1);
+        fib.push(1);
+
+        for (let i = 2; i < loopCount; i++){
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+
+        return fib[loopCount-1];
+    }
 };
 
 // Do not edit below this line
