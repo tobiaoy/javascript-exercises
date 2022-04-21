31923d581badcf4fb48c29be8a86a3e59c37283e
Tobi Oyero <tobioyero@gmail.com>
2022-04-21 15:17:06 -0400

    first attempt at palindromes.js and first test passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,34 @@
-const palindromes = function () {
+const palindromes = function (s) {
+//remove all whitespace
+let str = s.replace(/^\s+|\s+|\s+$/gm,'');
+
+//remove punctuation
+str = str.replace("[^\w\s]", '');
+
+//set string to lowercase
+str = str.toLowerCase();
+
+//create reverse string
+
+// -> split the string 
+split_str = str.split("");
+let reverse_str = [];
+
+// -> reverse the split string 
+for (let i = split_str.length; i >= 0; i--){
+    reverse_str.push(split_str[i]);
+}
+
+// -> put the string back together
+let rev_str = reverse_str.join("");
+
+
+//condition to check if the string and reverse string are the same
+if(str = rev_str){
+    return true;
+} else {
+    return false;
+}
 
 };
 
