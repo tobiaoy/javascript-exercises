627a8a165619e8974d7d63485009f5f94cbbd3d9
Tobi Oyero <tobioyero@gmail.com>
2022-04-21 11:38:05 -0400

    First attempt at leap years function and psased first two tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(number) {
+let num = number;
 
+if (num % 100 === 0){
+    return false;
+} else if (num % 4 === 0 || num % 400 === 0){
+    return true;
+} else {
+    return false;
+}
 };
 
 // Do not edit below this line
