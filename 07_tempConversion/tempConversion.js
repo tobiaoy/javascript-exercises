43645a650eccc33244d9f65a0faec098aa049e56
Tobi Oyero <tobioyero@gmail.com>
2022-04-21 12:45:39 -0400

    Fix return value on functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
+const ftoc = function(num) {
+let ftemp = num;
 
+let ctemp = (ftemp - 32) * 5/9;
+ctemp = ctemp.toFixed(1);
+
+return ctemp;
 };
 
-const ctof = function() {
+const ctof = function(num) {
+let ctemp = num;
 
+let ftemp = (ctemp * 9/5) + 32;
+ftemp = ftemp.toFixed(1);
+
+return ftemp;
 };
 
 // Do not edit below this line
