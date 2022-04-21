20c12d032ee584f12252f3926ae4929292c9c3b3
Tobi Oyero <tobioyero@gmail.com>
2022-04-21 13:20:04 -0400

    first attempt at calculator and passed add tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+
+  for (let i = 0; 1 < arr.length; i++){
+    sum += arr[i]; 
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(...theArgs) {
+  let res = 0;
 
+  for (let i = 0; i < theArgs.length; i++){
+    res *= theArgs[i];
+  }
+
+  return res;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	let res = Math.pow(num, power);
+
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let res;
+  
+  if (num === 0){
+    res = 1;
+  } else {
+    res *= factorial(num - 1);
+  }
+
+  return res;
 };
 
 // Do not edit below this line
