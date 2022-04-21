6c5d6e4f525c9d8701cfdc89d7d5ab30a5a041a5
Tobi Oyero <tobioyero@gmail.com>
2022-04-20 20:01:22 -0400

    removed the second loop checking for the higher value

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,24 +3,22 @@ let higher;
 let lower;
 let sum = 0;
 
+// if number is negative
+
+// if either value is not an int
+
 // loop determining the lower value
 for (let i = 0; i < moreArgs.length; i++){
     lower = moreArgs[0];
 
-    if (moreArgs[i] < lower){
-        lower = moreArgs[i];
+    if (moreArgs[1] < lower){
+        lower = moreArgs[1];
+        higher = moreArgs[0];
+    } else {
+        higher = moreArgs[1];
     }
 }
 
-// loop determining the higher value
-for (let i = 0; i < moreArgs.length; i++){
-higher = moreArgs[0];
-
-if (moreArgs[i] > higher){
-    higher = moreArgs[i];
-    }
-
-}
 
 // loop to sum values
 for (let i = lower; i <= higher; i++){
