b4d94eb474119bcd9760ee7543f8a6ab86f247b5
Tobi Oyero <tobioyero@gmail.com>
2022-05-04 18:49:34 -0400

    All tests passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(arr){
+ // const fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+ return arr.map(book => `${book.title}`);
+}    ;
 
 // Do not edit below this line
 module.exports = getTheTitles;
