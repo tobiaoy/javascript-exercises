b117471a1caae3774695ab794d8a4b5df0c2b398
Tobi Oyero <tobioyero@gmail.com>
2022-05-05 10:56:59 -0400

    1st and 3rd tests passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let older;
 
+    function getAge(obj){
+        let currYear = (new Date()).getFullYear;
+        
+        if (obj.yearOfDeath){
+        return obj.yearOfDeath - obj.yearOfBirth;
+        } else {
+          return currYear - obj.yearOfBirth;
+        }
+      };
+
+      const oldest = arr.reduce((previous, current) => {
+        //check to see if person has died
+        if (getAge(current) > getAge(previous)){
+          older = current;
+        } else {
+          older = previous;
+        }
+        
+        return older;
+      });
+      
+      return older;
 };
 
 // Do not edit below this line
